8e9d46ee4b6776a2e20eaeac80d22efdfb9a75bd
Jessamyn Smith <geekchick77@gmail.com>
2016-09-25 13:13:05 -0400

    Making class-based views work.

--- albums/urls.py
+++ albums/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.album_list, name="album_list"),
-    #url(r'^(?P<id>\d+)/$', views.album_details, name="album_details"
+    url(r'^$', views.AlbumListView.as_view(), name="album_list"),
+    url(r'^(?P<id>\d+)/$', views.AlbumDetailview.as_view(), name="album_details"),
 ]
